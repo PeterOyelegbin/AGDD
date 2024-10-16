583723f27338223564fe6d58449120dafd561197
Peter T. Oyelegbin <peteroyelegbin@gmail.com>
2024-10-16 16:02:01 +0100

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = ["127.0.0.1", "dap-alertgroup.com.ng", "www.dap-alertgroup.com.ng", "ndd.dap-alertgroup.com.ng"]
+ALLOWED_HOSTS = ["127.0.0.1", "dap-alertgroup.com.ng", "www.dap-alertgroup.com.ng", "ndd.dap-alertgroup.com.ng", "alert-direct-debit.onrender.com"]
 
 
 # Application definition
